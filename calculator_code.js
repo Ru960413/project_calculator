6da34feec141ee105456c58b287d8eaccdb5d94a
“Ru960413” <harleyquinzstyles@gmail.com>
2022-04-25 15:42:03 +0800

    tried to add btn through arr.

---
+++ calculator_code.js
@@ -0,0 +1,29 @@
+// const elements = [
+//   { buttonText: "AC" },
+//   { buttonText: "0" },
+//   { buttonText: "1" },
+//   { buttonText: "2" },
+//   { buttonText: "3" },
+//   { buttonText: "4" },
+//   { buttonText: "5" },
+//   { buttonText: "6" },
+//   { buttonText: "7" },
+//   { buttonText: "8" },
+//   { buttonText: "9" },
+//   { buttonText: "+" },
+//   { buttonText: "-" },
+//   { buttonText: "x" },
+//   { buttonText: "÷" },
+//   { buttonText: "=" },
+//   { buttonText: "%" },
+//   { buttonText: "." },
+//   { buttonText: "+/-" },
+// ];
+// const parent = document.getElementById("grid-container");
+// for (let i = 0; i < elements.length; i++) {
+//   let div = document.createElement("div");
+//   let button = document.createElement("button");
+//   button.innerHTML = elements[i].buttonText;
+//   div.appendChild(button);
+//   parent.appendChild(div);
+// }
